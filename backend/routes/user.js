ac371c11ffbcde4664e7ef50f88b763c10de327c
nader31 <nader.chebbo@gmail.com>
2022-10-11 06:43:32 +1000

    User authentication

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,4 +4,63 @@ const User = require('../models/user');
 
 const router = express.Router();
 
+router.post("/signup", (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hash
+        });
+        user.save()
+            .then(result => {
+                res.status(201).json({
+                    message: 'User created!',
+                    result: result
+                });
+            })
+            .catch(err => {
+                res.status(500).json({
+                    error: err
+                });
+            });
+    });
+});
+
+router.post("/login", (req, res, next) => {
+    let fetchedUser;
+    User.findOne({ email: req.body.email})
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            }
+            fetchedUser = user;
+            return bcrypt.compare(req.body.password, user.password);
+        })
+        .then(result => {
+            if (!result) {
+                return res.status(401).json({
+                    message:"Auth failed"
+                });
+            }
+            const token = jwt.sign(
+                {email: fetchedUser.email, userId: fetchedUser._id},
+                'secret_nader',
+                {expiresIn: "1h"}
+            );
+            res.status(200).json({
+                token: token,
+                expiresIn: 3600,
+                userId: fetchedUser._id
+            })
+        })
+        .catch(err => {
+            return res.status(401).json({
+                message:"Auth failed"
+            })
+        })
+});
+
 module.exports = router;
